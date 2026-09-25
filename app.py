93d883e0225f23aee999dd735592da91e6da2299
gitname <rnklxm2005@gmail.com>
2026-09-25 16:54:57 +0530

    Prepare app for Render deployment

--- app.py
+++ app.py
@@ -73,6 +73,12 @@ def prediction():
 # ======================================================
 
 if __name__ == "__main__":
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+
     app.run(
-        debug=True
+        host="0.0.0.0",
+        port=port,
+        debug=False
     )
